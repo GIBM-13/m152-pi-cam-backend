154c118a70b2335fb5137ff79966464765d8162f
Patrick Reimers <patrick@reimers.me>
2016-06-04 16:14:42 +0200

    Motion up and running

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ##########################################
-## @Author:	Patrick Reimers		##
-## @Version:	2016-05-10		##
+## @Author:     Patrick Reimers         ##
+## @Version:    2016-05-10              ##
 ##########################################
 
 echo -e "\e[32m-----------------------";
@@ -31,6 +31,13 @@ sudo raspi-config
 
 echo "";
 echo -e "\e[32m----------------------------";
+echo -e "\e[32m  Netwerk Proxy einrichten";
+echo -e "\e[32m----------------------------\e[0m";
+
+sleep 2;
+
+echo "";
+echo -e "\e[32m----------------------------";
 echo -e "\e[32mSystem wird aktualisiert und";
 echo -e "\e[32m  benötigtige Komponenten";
 echo -e "\e[32m    werden Installiert";
@@ -48,20 +55,19 @@ git clone https://github.com/GIBM-13/m152-pi-cam-backend.git pi-cam-files;
 sudo cp -R /tmp/pi-cam-files/files/etc/apt/* /etc/apt/;
 
 echo "";
-echo -e "\e[32m--------------------------";
-echo -e "\e[32mInstalliere PHP7 und NGINX";
-echo -e "\e[32m--------------------------\e[0m";
+echo -e "\e[32m----------------------------";
+echo -e "\e[32m Installiere PHP7 und NGINX";
+echo -e "\e[32m----------------------------\e[0m";
 
 curl http://www.linux-projects.org/listing/uv4l_repo/lrkey.asc | sudo apt-key add -;
 sudo apt-get update && sudo apt-get install -t stretch php7.0 php7.0-curl php7.0-gd php7.0-fpm php7.0-cli php7.0-opcache php7.0-mbstring php7.0-xml php7.0-zip nginx -y;
 
 echo "";
-echo -e "\e[32m----------------";
-echo -e "\e[32mInstalliere UV4L";
-echo -e "\e[32m----------------\e[0m";
+echo -e "\e[32m----------------------------";
+echo -e "\e[32m      Installiere UV4L";
+echo -e "\e[32m----------------------------\e[0m";
 
-sudo apt-get install uv4l -y;
-sudo apt get install uv4l-raspicam -y;
+sudo apt-get install uv4l uv4l-raspicam -y;
 sudo apt-get install uv4l-raspicam-extras -y;
 sudo apt-get install uv4l-server -y;
 sudo apt-get install uv4l-uvc -y;
@@ -69,66 +75,42 @@ sudo apt-get install uv4l-xscreen -y;
 sudo apt-get install uv4l-mjpegstream -y;
 
 echo "";
-echo -e "\e[32m------------------";
-echo -e "\e[32mInstalliere ffmpeg";
-echo -e "\e[32m------------------\e[0m";
-
-#cd /tmp;
-#wget https://github.com/ccrisan/motioneye/wiki/precompiled/ffmpeg_2.8.3.git325b593-1_armhf.deb;
-#sudo dpkg -i ffmpeg_2.8.3.git325b593-1_armhf.deb;
-#sudo apt-get remove libavcodec-extra-56 libavformat56 libavresample2 libavutil54 -y && sudo apt-get install libavutil54 libavformat56 libswscale3 -y;
-
-sudo apt-get install -t stretch ffmpeg libavdevice-ffmpeg56 libavfilter-ffmpeg5 libavformat-ffmpeg56 libgnutls30
-
-echo "";
-echo -e "\e[32m------------------";
-echo -e "\e[32mInstalliere motion";
-echo -e "\e[32m------------------\e[0m";
+echo -e "\e[32m----------------------------";
+echo -e "\e[32m     Installiere motion";
+echo -e "\e[32m----------------------------\e[0m";
 
 sudo apt-get install motion -y;
 
-cd /tmp;
-sudo wget https://github.com/ccrisan/motioneye/wiki/precompiled/motion-mrdave-raspbian -O /usr/local/bin/motion;
-sudo chmod +x /usr/local/bin/motion;
-
 echo "";
-echo -e "\e[32m----------------------";
-echo -e "\e[32mDateien werden kopiert";
-echo -e "\e[32m----------------------\e[0m";
+echo -e "\e[32m----------------------------";
+echo -e "\e[32m   Dateien werden kopiert";
+echo -e "\e[32m----------------------------\e[0m";
 
 sudo cp -R /tmp/pi-cam-files/files/* /;
 
 echo "";
-echo -e "\e[32m-----------------------";
-echo -e "\e[32mNGINX wird konfiguriert";
-echo -e "\e[32m-----------------------\e[0m";
+echo -e "\e[32m----------------------------";
+echo -e "\e[32m   NGINX wird konfiguriert";
+echo -e "\e[32m----------------------------\e[0m";
 
 cd /etc/nginx/sites-enabled/;
 sudo rm default;
 sudo ln -s ../sites-available/pi-cam;
 
 echo "";
-echo -e "\e[32m-------------------------";
-echo -e "\e[32mUV4L treiber wird geladen";
-echo -e "\e[32m-------------------------\e[0m";
+echo -e "\e[32m----------------------------";
+echo -e "\e[32m        Rechte setzen";
+echo -e "\e[32m----------------------------\e[0m";
 
-uv4l --driver raspicam --auto-video_nr;
-
-echo "";
-echo -e "\e[32m-------------";
-echo -e "\e[32mRechte setzen";
-echo -e "\e[32m-------------\e[0m";
-
-sudo chmod a+x /home/pi/start_motion.sh;
-sudo chmod a+x /home/pi/stop_motion.sh;
+sudo chmod a+x /home/pi/*.sh;
 sudo chown -R pi:pi /home/pi/;
 
 echo "";
-echo -e "\e[32m--------------------------";
-echo -e "\e[32mInstallation abgeschlossen";
-echo -e "\e[32m--------------------------\e[0m";
+echo -e "\e[32m----------------------------";
+echo -e "\e[32m Installation abgeschlossen";
+echo -e "\e[32m----------------------------\e[0m";
 
-# sudo apt-get install -t stretch ffmpeg libavdevice-ffmpeg56 libavfilter-ffmpeg5 libavformat-ffmpeg56 libopencv-core2.4v5 libopencv-imgproc2.4v5 libgnutls30
+sleep 2;
 
 echo "";
 echo -e "\e[32m----------------------------";
@@ -137,5 +119,5 @@ echo -e "\e[32m----------------------------\e[0m";
 
 sleep 5; #Sleep von 5 Sekunden, damit der Nutzer die Informationen lesen kann
 
-sudo shutdown -r now
+sudo shutdown -r now;
 exit;
